e60823cb857876cefa8cbce784dd36f765dda9e3
Prince stanley Jean baptiste <prstanley@Princes-MacBook-Air.local>
2025-06-21 15:16:02 -0400

    Add prometheus and grafana configs.

--- app/metrics.py
+++ app/metrics.py
@@ -1,7 +1,9 @@
-from prometheus_client import Counter, Histogram
+from prometheus_client import Counter, Histogram, Gauge
 import time
 from fastapi import Request
 from starlette.middleware.base import BaseHTTPMiddleware
+
+from app.fake_db import students, student_teacher_mapping
 
 REQUEST_COUNT = Counter(
     "api_request_count",
@@ -15,6 +17,16 @@ REQUEST_LATENCY = Histogram(
     ["endpoint"]
 )
 
+TOTAL_STUDENTS = Gauge(
+    "total_students",
+    "Nombre total d'étudiants dans fake_db"
+)
+
+TOTAL_MAPPINGS = Gauge(
+    "total_student_teacher_mappings",
+    "Nombre total de correspondances etudiants-professeur"
+)
+
 class MetricMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
         start_time = time.time()
@@ -26,3 +38,6 @@ class MetricMiddleware(BaseHTTPMiddleware):
         REQUEST_LATENCY.labels(endpoint).observe(process_time)
         return response
 
+def update_business_metrics():
+    TOTAL_STUDENTS.set(len(students))
+    TOTAL_MAPPINGS.set(len(student_teacher_mapping))
